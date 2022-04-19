e05c5d40f76168cb80ce214192143e539551a836
Iskren Ivanov <pecito_vn@hotmail.com>
2022-04-19 14:10:09 +0300

    Profile page added

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -16,8 +16,8 @@ export default function Post({post}) {
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
-                        <img className="postProfileImg" src={Users.filter(u => u.id == post?.userId)[0].profilePicture} alt="" />
-                        <span className="postUsername">{Users.filter(u => u.id == post?.userId)[0].username}</span>
+                        <img className="postProfileImg" src={Users.filter(u => u.id === post?.userId)[0].profilePicture} alt="" />
+                        <span className="postUsername">{Users.filter(u => u.id === post?.userId)[0].username}</span>
                         <span className="postDate">{post.date}</span>
                     </div>
                     <div className="postTopRight">
